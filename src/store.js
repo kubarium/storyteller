e5edd3e61ef04e49a02036eb6b51ed675782b374
kumsalo <kumsalo@theinfinitynetwork.com>
2019-01-31 17:59:42 -0500

    basic implementation

--- src/store.js
+++ src/store.js
@@ -1,16 +1,22 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
+import Vue from "vue";
+import Vuex from "vuex";
+//import markdown from "markdown/lib/markdown";
+import md from "markdown-it";
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    markdown: "",
+    preview: ""
   },
   mutations: {
-
+    updateMarkdown(state, value) {
+      console.log(value);
+      state.markdown = value;
+      //state.preview = markdown.toHTML(state.markdown);
+      state.preview = md({ html: true, linkify: true }).render(state.markdown);
+    }
   },
-  actions: {
-
-  }
-})
+  actions: {}
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,35 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <textarea /> -->
+    <v-textarea
+      label="Source"
+      @input="$store.commit('updateMarkdown',$event)"
+      hint="Mark it down here"
+    />
+    <v-spacer />
+    <div
+      class="preview"
+      v-html="$store.state.preview"
+    />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+//import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
-  name: 'home',
+  name: "home",
   components: {
-    HelloWorld
+    //HelloWorld
   }
-}
+};
 </script>
+
+<style lang="scss" scoped>
+.preview {
+  min-height: 100vh;
+  min-width: 50vw;
+  outline: 1px solid red;
+}
+</style>
