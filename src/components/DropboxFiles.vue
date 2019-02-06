bf86a2b87fb3e9bbbf6f35e84a9e897a23944f72
kumsalo <kumsalo@theinfinitynetwork.com>
2019-02-06 17:45:37 -0500

    lists dropbox files and folders with breadcrumbs, needs polishing

--- src/components/DropboxFiles.vue
+++ src/components/DropboxFiles.vue
@@ -2,6 +2,7 @@
   <v-dialog
     v-model="dialog"
     width="500"
+    scrollable
   >
     <v-btn
       slot="activator"
@@ -11,15 +12,45 @@
     </v-btn>
 
     <v-card>
-      <v-card-title
-        class="headline grey lighten-2"
-        primary-title
-      >
+      <v-card-title>
         Your Dropbox Files
       </v-card-title>
-
+      <v-divider />
+      <v-breadcrumbs :items="breadcrumbs">
+        <template
+          slot="item"
+          slot-scope="props"
+        >
+          <!-- <a :href="props.item.href">{{ props.item.text }}</a> -->
+          <span @click="getEntries(props.item.path)">{{props.item.text}}</span>
+        </template>
+      </v-breadcrumbs>
+      <v-divider />
       <v-card-text>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+        <v-list>
+          <template v-for="(entry, index) in entries">
+
+            <v-list-tile
+              :key="entry.title"
+              @click="getEntries(entry.path_lower)"
+            >
+              <v-list-tile-avatar>
+                <v-icon v-if="entry['.tag']==='folder'">folder</v-icon>
+                <v-icon v-else-if="entry['.tag']==='parent'">arrow_upward</v-icon>
+                <v-icon v-else>edit</v-icon>
+              </v-list-tile-avatar>
+
+              <v-list-tile-content>
+                <v-list-tile-title v-html="entry.name"></v-list-tile-title>
+              </v-list-tile-content>
+            </v-list-tile>
+
+            <v-divider
+              v-show="index < entries.length - 1"
+              :key="index"
+            ></v-divider>
+          </template>
+        </v-list>
       </v-card-text>
 
       <v-divider></v-divider>
@@ -31,7 +62,7 @@
           flat
           @click="dialog = false"
         >
-          I accept
+          Close
         </v-btn>
       </v-card-actions>
     </v-card>
@@ -39,31 +70,66 @@
 </template>
 
 <script>
-//require("dotenv").config();
-//const Dotenv = require("dotenv-webpack");
-//a = new Dotenv();
-//import Dropbox from "dropbox";
-console.log(process.env);
+import Dropbox from "dropbox";
 
-//console.log(a);
 export default {
   data() {
     return {
-      dialog: false
+      dialog: false,
+      dbx: null,
+      path: "",
+      entries: null,
+      breadcrumbs: []
     };
   },
+  methods: {
+    getEntries(path) {
+      //console.log(evt);
+      this.path = path;
+
+      const breadcrumbsPath = this.path.split("/").reduce((a, b, index) => {
+        return index > 0 ? a.concat(`${a[index - 1]}/${b}`) : a.concat(b);
+      }, []);
+      this.breadcrumbs = this.path.split("/").map((path, index) => {
+        return {
+          text: path,
+          path: breadcrumbsPath[index]
+        };
+      });
+      this.dbx
+        .filesListFolder({ path })
+        .then(response => this.fileTree(response.entries))
+        .catch(error => console.error(error));
+    },
+    fileTree(entries) {
+      this.entries = entries.filter(entry => {
+        if (entry[".tag"] === "folder") {
+          return true;
+        }
+        if (RegExp(/\.(.*?)$/).exec(entry.name)[1] === "md") {
+          return true;
+        }
+        return false;
+      });
+
+      if (this.path !== "") {
+        //console.log(this.path.replace(RegExp(/\/.*?$/), ""));
+        this.entries.unshift({
+          name: "Go up",
+          ".tag": "parent",
+          path_lower: this.path.replace(RegExp(/\/\w*$/g), "")
+        });
+      }
+
+      console.log(entries);
+    },
+    initialize() {}
+  },
   created() {
-    /* var dbx = new Dropbox.Dropbox({
-      accessToken: this.access_token
+    this.dbx = new Dropbox.Dropbox({
+      accessToken: process.env.VUE_APP_DROPBOX_ACCESS_TOKEN
     });
-    dbx
-      .filesListFolder({ path: "" })
-      .then(function(response) {
-        console.log(response.entries);
-      })
-      .catch(function(error) {
-        console.error(error);
-      }); */
+    this.getEntries(this.path);
   }
 };
 </script>
