3370f134fddb14a24211197e2333f9e498bdd092
kubarium <iam@denizkumsal.com>
2019-02-27 23:54:23 -0500

    implements basic content operation

--- src/store/content.js
+++ src/store/content.js
@@ -1,3 +1,5 @@
+import {addPageBreak} from "./utils"
+
 export default {
     state : {},
     getters : {
@@ -23,10 +25,28 @@ export default {
             }
         }
     },
-    mutations : {
-        addPageBreak(state) {
-            state.markdown.content = "asd";
+    mutations : {},
+    actions : {
+        addPageBreak({commit, rootState}) {
+            //this is force updating the source textarea with the injected content
+            var textarea = document.getElementById("source");
+            console.log(rootState)
+
+            var startPos = textarea.selectionStart;
+            var endPos = textarea.selectionEnd;
+            var currentValue = rootState.file.markdown.content;
+            var injected = addPageBreak;
+            if (textarea.selectionStart || textarea.selectionStart == "0") {
+                commit("updateMarkdown", currentValue.substring(0, startPos) + injected + currentValue.substring(endPos, currentValue.length))
+                //rootState.file.markdown.content = ;
+            } else {
+                //if there is a selection, ignore it and append content
+                commit("updateMarkdown", rootState.file.markdown.content + injected);
+            }
+            //focus again at the end of injected content
+            textarea.focus();
+            textarea.selectionStart = startPos + injected.length;
+            textarea.selectionEnd = startPos + injected.length;
         }
-    },
-    actions : {}
+    }
 };
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -69,30 +69,6 @@ export default {
     ContentOperations,
     Preview
   },
-  methods: {
-    insertAtCursor(injected = "") {
-      //this is force updating the source textarea with the injected content
-
-      var textarea = document.getElementById("source");
-
-      var startPos = textarea.selectionStart;
-      var endPos = textarea.selectionEnd;
-      var currentValue = textarea.value;
-      if (textarea.selectionStart || textarea.selectionStart == "0") {
-        textarea.value =
-          currentValue.substring(0, startPos) +
-          injected +
-          currentValue.substring(endPos, currentValue.length);
-      } else {
-        //if there is a selection, ignore it and append content
-        textarea.value += injected;
-      }
-      //focus again at the end of injected content
-      textarea.focus();
-      textarea.selectionStart = startPos + injected.length;
-      textarea.selectionEnd = startPos + injected.length;
-    }
-  },
   data() {
     return {
       publicPath: process.env.BASE_URL
