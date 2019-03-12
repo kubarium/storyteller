c46fbb2883d1c2e432ff26d7b30d47d6b62f1ad2
kumsalo <kumsalo@theinfinitynetwork.com>
2019-03-12 17:56:34 -0400

    switch to fetch

--- src/store/thesaurus.js
+++ src/store/thesaurus.js
@@ -29,8 +29,15 @@ export default {
   mutations: {},
   actions: {
     getThesaurus({ state }, word) {
-      return axios
-        .get(`${state.urls.thesaurus}${word}/synonyms;antonyms`, state.options)
+      return fetch(`${state.urls.thesaurus}${word}/synonyms;antonyms`, {
+        method: "GET",
+        mode: "no-cors",
+        headers: {
+          app_id: process.env.VUE_APP_OXFORD_DICTIONARY_APP_ID,
+          app_key: process.env.VUE_APP_OXFORD_DICTIONARY_APP_KEY
+        }
+      })
+        .then(response => response.json())
         .then(data => {
           var senses = data.results[0].lexicalEntries
             .map(le => le.entries[0].senses)
@@ -62,6 +69,39 @@ export default {
             antonyms: antonyms.concat(subsensicalAntonyms)
           };
         });
+      /* return axios
+        .get(`${state.urls.thesaurus}${word}/synonyms;antonyms`, state.options)
+        .then(data => {
+          var senses = data.results[0].lexicalEntries
+            .map(le => le.entries[0].senses)
+            .flat();
+          var synonyms = senses
+            .map(sense => sense.synonyms)
+            .flat()
+            .filter(subsense => subsense);
+          var antonyms = senses
+            .map(sense => sense.antonyms)
+            .flat()
+            .filter(subsense => subsense);
+
+          var subsenses = senses
+            .map(sense => sense.subsenses)
+            .flat()
+            .filter(subsense => subsense);
+          var subsensicalSynonyms = subsenses
+            .map(subsense => subsense.synonyms)
+            .flat()
+            .filter(subsense => subsense);
+          var subsensicalAntonyms = subsenses
+            .map(subsense => subsense.antonyms)
+            .flat()
+            .filter(subsense => subsense);
+
+          return {
+            synonyms: synonyms.concat(subsensicalSynonyms),
+            antonyms: antonyms.concat(subsensicalAntonyms)
+          };
+        }); */
     }
   }
 };
