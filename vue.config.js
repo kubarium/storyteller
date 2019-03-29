9e4fca9d8fd87dc7f743d813bd461a8b1e3c6d06
kubarium <iam@denizkumsal.com>
2019-03-29 18:09:49 -0400

    adds baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  baseUrl: process.env.NODE_ENV === 'production'
+    ? '/storyteller/'
+    : '/',
   publicPath: process.env.NODE_ENV === 'production'
     ? '/storyteller/'
     : '/'
