fa819556299813442db8c22d92b06885c98434cf
kumsalo <kumsalo@theinfinitynetwork.com>
2019-06-03 14:43:12 -0400

    Updates

---
+++ styles/compiler.js
@@ -0,0 +1,30 @@
+const sass = require("node-sass");
+const argv = require("yargs").argv;
+const fs = require("fs");
+const path = require("path");
+
+//console.clear();
+const file = path.resolve(argv.file);
+const folder = RegExp(/\/.*\//).exec(file)[0];
+const css = `${file}.css`;
+const scss = `${file}.scss`;
+
+let data = fs.readFileSync(scss, { encoding: "utf8" });
+data = `$root: "/${argv.env === "production" ? "storyteller/" : ""}"; ${data}`;
+
+sass.render(
+  {
+    data,
+    sourceMap: true,
+    outFile: css,
+    includePaths: [folder]
+  },
+  (err, result) => {
+    if (!err) {
+      // No errors during the compilation, write this result on the disk
+      fs.writeFileSync(css, result.css);
+      fs.writeFileSync(`${css}.map`, result.map);
+      console.log(`Rendered ${scss} to ${css}`);
+    } else console.log(err);
+  }
+);
