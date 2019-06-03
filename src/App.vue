30875a73ab8513ec5e5ba1e129745f7e56dd9956
kumsalo <kumsalo@theinfinitynetwork.com>
2019-06-03 17:23:05 -0400

    intrdoces double click on preview as a refresh alternative

--- src/App.vue
+++ src/App.vue
@@ -6,26 +6,36 @@
       </v-toolbar-title>
       <v-spacer></v-spacer>
       <transition name="fade">
-        <v-chip color="teal" text-color="white" v-if="$store.state.markdown.saving">
+        <v-chip
+          color="teal"
+          text-color="white"
+          v-if="$store.state.markdown.saving"
+        >
           Saved
           <v-icon right>check_circle</v-icon>
         </v-chip>
-        <v-chip color="red" text-color="white" v-if="$store.state.markdown.modified">
+        <v-chip
+          color="red"
+          text-color="white"
+          v-if="$store.state.markdown.modified"
+        >
           Edited
           <v-icon right>priority_high</v-icon>
         </v-chip>
       </transition>
-      <v-chip label v-show="$store.state.markdown.path">{{ $store.state.markdown.path }}</v-chip>
-      <v-divider vertical/>
-      <settings/>
-      <dropbox-files/>
-      <thesaurus/>
-      <table-of-contents/>
+      <v-chip label v-show="$store.state.markdown.path">{{
+        $store.state.markdown.path
+      }}</v-chip>
+      <v-divider vertical />
+      <settings />
+      <dropbox-files />
+      <thesaurus />
+      <table-of-contents />
       <!-- <image-selector/> -->
     </v-toolbar>
 
     <v-content>
-      <router-view/>
+      <router-view />
     </v-content>
     <link
       :disabled="$store.state.preview.useCustomStyle"
@@ -35,9 +45,11 @@
         }.css?v=${$store.state.preview.styleVersion}`
       "
       rel="stylesheet"
-    >
+    />
 
-    <v-style :disabled="!$store.state.preview.useCustomStyle">{{ $store.state.preview.customStyle }}</v-style>
+    <v-style :disabled="!$store.state.preview.useCustomStyle">{{
+      $store.state.preview.customStyle
+    }}</v-style>
   </v-app>
 </template>
 
@@ -61,6 +73,11 @@ export default {
     Thesaurus,
     TableOfContents
     /* ImageSelector */
+  },
+  mounted() {
+    document.querySelector(".pages").addEventListener("dblclick", () => {
+      this.$store.dispatch("applyStyle", this.$store.getters.activeStyle);
+    });
   }
 };
 </script>
