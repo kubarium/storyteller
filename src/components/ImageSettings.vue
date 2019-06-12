6d08d9ed267fd0bc9a55bc5d67b9d4d81b05720f
kumsalo <kumsalo@theinfinitynetwork.com>
2019-06-12 17:00:11 -0400

    adds more user interface elements

--- src/components/ImageSettings.vue
+++ src/components/ImageSettings.vue
@@ -7,66 +7,227 @@
     @input="$store.dispatch('refreshImages')"
   >
     <v-card>
-      <v-card-title
-        >Image Settings
-        <!-- <v-spacer />
-        <v-btn icon flat @click="$store.dispatch('refreshImages')">
-          <v-icon>refresh</v-icon>
-        </v-btn> -->
-      </v-card-title>
+      <v-card-title>Image Settings </v-card-title>
       <v-divider />
-      <!-- <v-breadcrumbs :items="$store.state.images.breadcrumbs">
-        <template slot="item" slot-scope="props">
-          <a @click="clickEntry(props.item)">{{ props.item.text }}</a>
-        </template>
-      </v-breadcrumbs>
-      <v-divider /> -->
-      <v-card-text style="height:500px">
-        <!-- <v-list>
-          <template v-for="(entry, index) in $store.state.images.entries">
-            <v-divider v-show="index > 0" :key="index"></v-divider>
+      <v-card-text style="height:600px">
+        <v-container grid-list-lg class="px-0 py-2">
+          <v-layout>
+            <v-flex xs8>
+              <v-radio-group
+                label="Position"
+                row
+                hide-details
+                v-model="$store.state.imageSettings.styles.position"
+              >
+                <v-radio
+                  v-for="position in positions"
+                  :key="position"
+                  :label="`${position}`"
+                  :value="position"
+                ></v-radio>
+              </v-radio-group>
+            </v-flex>
+            <v-flex xs4>
+              <v-select
+                :items="modes"
+                hide-details
+                v-model="$store.state.imageSettings.styles['mix-blend-mode']"
+                label="Blend Mode"
+              />
+            </v-flex>
+          </v-layout>
+        </v-container>
 
-            <v-list-tile
-              :disabled="$store.state.images.fetching"
-              :key="entry.title"
-              @click="clickEntry(entry)"
-            >
-              <v-list-tile-avatar>
-                <v-icon v-if="entry['.tag'] === 'folder'">folder</v-icon>
-                <v-icon v-else-if="entry['.tag'] === 'parent'"
-                  >arrow_upward</v-icon
-                >
-                <v-icon v-else>image</v-icon>
-              </v-list-tile-avatar>
+        <v-container grid-list-lg class="px-0 py-2">
+          <v-layout>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Width"
+                    v-model="$store.getters.imageWidth"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imageWidthUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Height"
+                    v-model="$store.getters.imageHeight"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imageHeightUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
 
-              <v-list-tile-content>
-                <v-list-tile-title v-html="entry.name"></v-list-tile-title>
-              </v-list-tile-content>
+        <v-container grid-list-lg class="px-0 py-2">
+          <v-layout>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Top"
+                    v-model="$store.getters.imagePositionTop"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imagePositionTopUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Bottom"
+                    v-model="$store.getters.imagePositionBottom"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imagePositionBottomUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
 
-              <v-list-tile-action v-if="entry['.tag'] === 'file'">
-                <v-btn
-                  :disabled="$store.state.images.fetching"
-                  icon
-                  @click.stop="
-                    $store.dispatch('deleteFromDropbox', entry.path_lower)
-                  "
-                >
-                  <v-icon>delete_forever</v-icon>
-                </v-btn>
-              </v-list-tile-action>
-            </v-list-tile>
-          </template>
-        </v-list> -->
-        <v-textarea
-          :value="$store.state.imageSettings.url"
-          label="Image URL"
-          no-resize
-        ></v-textarea>
-        <v-select
-          :items="modes"
-          v-model="$store.state.imageSettings.styles['mix-blend-mode']"
-          label="Blend Mode"
-        ></v-select>
+        <v-container grid-list-lg class="px-0 py-2">
+          <v-layout>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Left"
+                    v-model="$store.getters.imagePositionLeft"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imagePositionLeftUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+            <v-flex xs6>
+              <v-layout>
+                <v-flex xs6>
+                  <v-text-field
+                    type="number"
+                    label="Right"
+                    v-model="$store.getters.imagePositionRight"
+                    hide-details
+                  />
+                </v-flex>
+                <v-flex xs6>
+                  <v-radio-group
+                    row
+                    mandatory
+                    hide-details
+                    v-model="$store.getters.imagePositionRightUnit"
+                  >
+                    <v-radio
+                      v-for="unit in units"
+                      :key="unit"
+                      :label="`${unit}`"
+                      :value="unit"
+                    ></v-radio>
+                  </v-radio-group>
+                </v-flex>
+              </v-layout>
+            </v-flex>
+          </v-layout>
+        </v-container>
+
+        <v-container class="px-0 py-2">
+          <v-divider />
+          <v-textarea
+            :value="$store.state.imageSettings.url"
+            label="Image URL"
+            no-resize
+          />
+          <v-textarea
+            :value="$store.state.imageSettings.styles.other"
+            label="Other Styles"
+            no-resize
+          />
+        </v-container>
       </v-card-text>
 
       <v-divider></v-divider>
@@ -85,11 +246,12 @@
 </template>
 
 <script>
-//normal|multiply|screen|overlay|darken|lighten|color-dodge|color-burn|difference|exclusion|hue|saturation|color|luminosity
 export default {
   name: "image-settings",
   data() {
     return {
+      units: "%|px".split("|"),
+      positions: "initial|absolute|relative".split("|"),
       modes: "normal|multiply|screen|overlay|darken|lighten|color-dodge|color-burn|difference|exclusion|hue|saturation|color|luminosity".split(
         "|"
       )
--- src/store/imageSettings.js
+++ src/store/imageSettings.js
@@ -22,23 +22,55 @@ export default {
         filter: "",
         breadcrumbs: [] */
   },
+  getters: {
+    imageWidth: state => (state.styles.width && RegExp(/\d+/, "g").exec(state.styles.width)[0]) || "",
+    imageHeight: state => (state.styles.height && RegExp(/\d+/, "g").exec(state.styles.height)[0]) || "",
+    imageWidthUnit: state => state.styles.width.split(RegExp(/\d+/, "g"))[1] || "px",
+    imageHeightUnit: state => state.styles.height.split(RegExp(/\d+/, "g"))[1] || "px",
+
+    imagePositionLeft: state => (state.styles.left && RegExp(/\d+/, "g").exec(state.styles.left)[0]) || "",
+    imagePositionLeftUnit: state => state.styles.left.split(RegExp(/\d+/, "g"))[1] || "px",
+
+    imagePositionRight: state => (state.styles.right && RegExp(/\d+/, "g").exec(state.styles.right)[0]) || "",
+    imagePositionRightUnit: state => state.styles.right.split(RegExp(/\d+/, "g"))[1] || "px",
+
+    imagePositionTop: state => (state.styles.top && RegExp(/\d+/, "g").exec(state.styles.top)[0]) || "",
+    imagePositionTopUnit: state => state.styles.top.split(RegExp(/\d+/, "g"))[1] || "px",
+
+    imagePositionBottom: state => (state.styles.bottom && RegExp(/\d+/, "g").exec(state.styles.bottom)[0]) || "",
+    imagePositionBottomUnit: state => state.styles.bottom.split(RegExp(/\d+/, "g"))[1] || "px"
+  },
   mutations: {
     toggleImageSettings(state, toggle) {
       state.dialog = toggle;
     },
-    /* toggleImageSelector(state, toggle) {
-      state.selector.dialog = toggle;
-    }, */
+    updateStyles(state, styles) {
+      console.log(Object.assign({}, state.styles, styles));
+      state.styles = Object.assign({}, state.styles, styles);
+      //[style.key] = style.value;
+    },
     updateSettings(state, setting) {
       state[setting.key] = setting.value;
     }
   },
   actions: {
-    getImageSettings({ state, commit }, img) {
+    getImageSettings({ commit }, img) {
       const url = RegExp(/src="(.*?)"/).exec(img)[1];
       console.log(url);
       commit("updateSettings", { key: "url", value: url });
-      const style = RegExp(/style="(.*?)"/).exec(img)[1];
+      const styles = RegExp(/style="(.*?)"/)
+        .exec(img)[1]
+        .replace(RegExp(/ /, "g"), "")
+        .split(";");
+
+      let delir = styles.reduce((result, item) => {
+        const entry = item.split(":");
+
+        result[entry[0]] = entry[1];
+        return result;
+      }, {});
+      commit("updateStyles", delir);
+      //commit("updateStyles", { key: "url", value: url });
 
       //state.settings;
     }
