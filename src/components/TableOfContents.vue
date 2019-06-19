6ca587fe1b55df6fc800e4095d457c1aa9e2fde0
kumsalo <kumsalo@theinfinitynetwork.com>
2019-06-19 12:01:51 -0400

    favors treeview over list to display items

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,31 +1,36 @@
 <template>
-  <v-dialog v-model="$store.state.preview.toc" class="toc" scrollable :max-width="width">
+  <v-dialog
+    v-model="$store.state.preview.toc"
+    class="toc"
+    scrollable
+    :max-width="width"
+  >
     <v-card>
       <v-card-title>Table of Contents</v-card-title>
-      <v-card-text class="pa-0" style="height:480px">
-        <v-list dense>
-          <template v-for="(entry, index) in TOC">
-            <v-divider :key="index"/>
-            <v-list-tile :key="`entry-${index}`">
-              <v-list-tile-content>
-                <a
-                  @click="scrollToHeader(entry.scrollTop)"
-                  :class="{inset:entry.header.nodeName!='H1'}"
-                >
-                  {{
-                  entry.textContent
-                  }}
-                </a>
-              </v-list-tile-content>
-            </v-list-tile>
+      <v-card-text style="height:480px">
+        <v-treeview :items="TOC" open-on-click hoverable open-all>
+          <template slot="label" slot-scope="{ item }">
+            <div
+              class="table-of-contents-item"
+              @click="scrollToHeader(item.scrollTop)"
+            >
+              <span>
+                {{ item.name }}
+              </span>
+              <span>
+                {{ item.scrollTop }}
+              </span>
+            </div>
           </template>
-        </v-list>
+        </v-treeview>
       </v-card-text>
 
-      <v-divider/>
+      <v-divider />
       <v-card-actions>
-        <v-spacer/>
-        <v-btn color="primary" flat @click="$store.commit('toggleTOC', false)">Close</v-btn>
+        <v-spacer />
+        <v-btn color="primary" flat @click="$store.commit('toggleTOC', false)"
+          >Close</v-btn
+        >
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -55,26 +60,48 @@ export default {
     updateTOC() {
       const h1 = Array.from(document.querySelectorAll(".pages h1"));
       const h2 = Array.from(document.querySelectorAll(".pages h2"));
-      const h3 = Array.from(document.querySelectorAll(".pages h3"));
+      //const h3 = Array.from(document.querySelectorAll(".pages h3"));
 
       const headers = h1
-        .concat(h2, h3)
+        .concat(h2)
         .sort((a, b) =>
           a.parentElement.offsetTop > b.parentElement.offsetTop
             ? 1
             : a.parentElement.offsetTop < b.parentElement.offsetTop
             ? -1
             : 0
-        );
+        )
+        .reduce((result, header) => {
+          if (header.nodeName === "H1") {
+            return result.concat([
+              {
+                children: [],
+                name: header.textContent,
+                scrollTop: header.parentElement.offsetTop
+              }
+            ]);
+          } else {
+            result[result.length - 1]["children"].push({
+              name: header.textContent,
+              scrollTop: header.parentElement.offsetTop,
+              pageNumber: getComputedStyle(
+                header.parentElement.querySelector(`.pageNumber.auto`),
+                ":after"
+              ).getPropertyValue("content")
+            });
 
-      return headers.map(header => {
-        //console.log(header.parentElement);
-        return {
-          header,
-          textContent: header.textContent,
-          scrollTop: header.parentElement.offsetTop
-        };
-      });
+            console.log(
+              getComputedStyle(
+                header.parentElement.querySelector(`.pageNumber.auto`),
+                ":after"
+              ).getPropertyValue("counter")
+            );
+
+            return result;
+          }
+        }, []);
+
+      return headers;
     },
     scrollToHeader(scrollTop) {
       this.$store.commit("toggleTOC", false);
@@ -85,7 +112,9 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.inset {
-  text-indent: 2em;
+.table-of-contents-item {
+  display: grid;
+  grid-auto-flow: column;
+  justify-content: space-between;
 }
 </style>
