9385361f69c93eec675abbf28619fbe18312592b
kubarium <iam@denizkumsal.com>
2019-06-22 15:52:06 -0400

    Updates

--- styles/compiler.js
+++ styles/compiler.js
@@ -6,7 +6,7 @@ const path = require("path");
 //console.clear();
 if (!argv.file) return console.log(`Run it like node compiler.js --file=5E/5E`);
 const file = path.resolve(argv.file);
-const folder = RegExp(/\/.*\//).exec(file)[0];
+const folder = RegExp(/.*(\\|\/)/).exec(file)[0];
 const css = `${file}.css`;
 const scss = `${file}.scss`;
 
