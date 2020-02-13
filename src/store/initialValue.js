a933075df78ee8b97cf00fcf3650ee51b097917a
Kumsal Obuz <info@viroidgames.com>
2020-02-12 19:22:46 -0500

    sanitizes the language

--- src/store/initialValue.js
+++ src/store/initialValue.js
@@ -1,22 +1,24 @@
-export const initialValue = `# Open a file and edit the hack out of it!
+export const initialValue = `<div class="wide">
+
+# Open a file and edit the hack out of it!
 
 ## How do I start?
 
 Hit the cog icon on the top right to bring up an interface where you can enter your Dropbox Access Token. You need to create a Dropbox App so Storyteller could store files in your personal account.
 
-## I can't edit shit?
+## Why can't I edit?
 
-That's because you're stubborn. Open a file using the **File** menu or hit CTRL+O, and then this area will be editable. Also, have you wired your Dropbox Account?
+Open a file using the **File** menu or hit CTRL+O, and then this area will be editable. Also, have you wired your Dropbox Account?
 
 ## I can't use the Markdown menu?!
 
-Sigh! Have you seen the sentence above? 
+Sigh! Have you read the sentence above? 
 
 ## I'm starting to like this, what now?
 
 Keep editing and don't forget to save your progress by hitting CTRL+S or using **File** menu. Additionally, you can use CTRL+B and CTRL+I to make any selected text bold and italic respectively.
 
-## Can I print this shit?
+## Can I print this stuff?
 
 Oh yeah! You can either choose to print it with all images included which means you'll **Show No Mercy** to your printer ink cartridge, or you could go **Ink Friendly**.
 
@@ -27,4 +29,5 @@ You're in luck friend. Use the **Size** menu to select the appropriate format. R
 ## I have made a mistake
 
 We all do, hang on. Hit CTRL+F12 or Revert from the **File** menu. Your file content from its last version will be here. If you just saved it though, reverting will have no effect. Remember, you can always use your Dropbox account to make changes to your files and simply open later here.
+</div>
 `
