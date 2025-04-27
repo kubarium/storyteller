306915de9ea0ba58a022e1f042bee5f14b541420
Kumsal Obuz <iam@denizkumsal.com>
2025-04-27 13:03:22 -0400

    cleans the slate for vite-vue3 version

--- README.md
+++
@@ -1,5 +0,0 @@
-# Storyteller
-
-Still WIP fancy markdown editor!
-
-[![storyteller.png](https://i.postimg.cc/tTF6NjrR/storyteller.png)](https://postimg.cc/k6XD7k3L)
--- src/store/cloudinary.js
+++
@@ -1,8 +0,0 @@
-import cloudinary from "cloudinary-core";
-
-var cl = new cloudinary.Cloudinary({ cloud_name: "viroidgames", secure: false });
-cl.window.cl = cl;
-window.cloudinary = cloudinary;
-export default {
-  state: {}
-};
