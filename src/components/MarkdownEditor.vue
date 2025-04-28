e69505d6c3e9f5279c0016c79cc45b38998ad84d
Kumsal Obuz <iam@denizkumsal.com>
2025-04-28 15:49:46 -0400

    Adds codemirror as a separate component

---
+++ src/components/MarkdownEditor.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { useEditorStore } from '@/stores/editor';
+import { markdown } from '@codemirror/lang-markdown';
+import { Codemirror } from 'vue-codemirror';
+import { EditorView } from '@codemirror/view';
+
+const editorStore = useEditorStore()
+
+const extensions = [markdown(), EditorView.lineWrapping]
+
+// Codemirror EditorView instance ref
+const view = shallowRef()
+const handleReady = (payload: object) => {
+  view.value = payload.view
+  console.log(view.value);
+}
+
+// Status is available at all times via Codemirror EditorView
+const getCodemirrorStates = () => {
+  const state = view.value.state
+  const ranges = state.selection.ranges
+  const selected = ranges.reduce((r, range) => r + range.to - range.from, 0)
+  const cursor = ranges[0].anchor
+  const length = state.doc.length
+  const lines = state.doc.lines
+  // more state info ...
+  // return ...
+}
+//@change="console.log('change', $event)" @focus="console.log('focus', $event)" @blur="console.log('blur', $event)"
+onMounted(() => {
+  // console.log(EditorView);
+
+})
+</script>
+<template>
+  <Codemirror v-model="editorStore.code" placeholder="Code goes here..." :extensions="extensions" :autofocus="true"
+    :indent-with-tab="true" :tab-size="2" @ready="handleReady" style="height: 100%;" />
+</template>
