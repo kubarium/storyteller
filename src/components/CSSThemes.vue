329803e8ee0d6f4034d17eb86945df7ddc29728f
Kumsal Obuz <iam@denizkumsal.com>
2025-04-29 16:18:27 -0400

    Adds components to toolbars, markdown and preview.

---
+++ src/components/CSSThemes.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { usePreviewStore } from '@/stores/preview';
+
+
+const preview = usePreviewStore()
+const change_theme = (theme: object) => {
+  console.log('Changing theme to', preview.theme);
+
+  preview.theme = theme
+}
+</script>
+<template>
+  <SplitButton label="CSS Themes" :model="preview.CSS_THEMES" size="small">
+    <template #item="{ item }">
+      <a @click="change_theme(item)" class="p-2 items-center inline-flex w-full h-9">{{ item.label }}</a>
+    </template>
+  </SplitButton>
+
+</template>
